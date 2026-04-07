436d619f8e9d1556717d5e507c0e7800afe949b2
Maxy <maverickcagne@gmail.com>
2026-04-07 08:18:31 +0800

    email latest

--- client/src/lib/emailjs.js
+++ client/src/lib/emailjs.js
@@ -1,6 +1,5 @@
 import emailjs from "@emailjs/browser";
 
-// Double check these names match your .env file!
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
